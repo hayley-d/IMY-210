12928e47357992d10a8d93b8cd8abd1d18b1b848
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-02-27 13:16:37 +0200

    Initial component upload

---
+++ Practical-1/src/components/Input.vue
@@ -0,0 +1,78 @@
+<!--Hayley Dodkins u21528790-->
+<template>
+  <div class = "input-container">
+      <label for="movie-input">Enter a Movie Title</label>
+      <input type="text" id = "text-input" name="movie-input" v-model="movieName" :class="{ 'full': isFull }" @keyup.enter="addMovie" placeholder="Enter movie title" ></input>
+      <button id = "add-btn" @click="addMovie">Add to List</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    props: ['isFull'],
+    data() {
+        return {
+            movieName: '',
+
+        };
+    },
+    methods: {
+        addMovie() {
+            if (this.movieName.trim() !== '') {
+                this.$emit('addMovie', this.movieName.trim());
+                this.movieName = '';
+
+                // Emit an event to the parent when the input is full
+                if (this.isFull) {
+                    this.$emit('inputFull');
+                }
+            }
+        },
+    },
+};
+</script>
+
+<style scoped>
+  #text-input{
+      border-radius: 10px;
+      font-size: 20px;
+      padding:10px;
+      border:none;
+      outline: none;
+  }
+
+  button{
+      border-radius: 10px;
+      border:none;
+      outline: none;
+      font-size: 20px;
+      padding:10px;
+      cursor:pointer;
+      background-color: palevioletred;
+      color:white;
+  }
+
+  button:hover{
+      background-color: darkgrey;
+  }
+
+  .input-container{
+      display: flex;
+      gap:20px;
+      justify-content: center;
+      align-items: center;
+      width:50%;
+      height:30vh;
+
+  }
+
+  label{
+      font-size: 20px;
+      color: white;
+  }
+
+  .full {
+      border: 2px solid red;
+  }
+</style>
---
+++ Practical-1/src/components/List.vue
@@ -0,0 +1,59 @@
+<!--Hayley Dodkins u21528790-->
+<template>
+<div class = "list-container">
+    <ol>
+        <li v-for="(movie,index) in movies" :key="index">
+            {{movie}}
+            <button @click="removeMovie(index)">-</button>
+        </li>
+    </ol>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    props: ['movies'],
+    methods: {
+        removeMovie(index) {
+            this.$emit('removeMovie', index);
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+*{
+    font-family: Roboto,sans-serif;
+    overflow-x: hidden;
+}
+
+.list-container{
+    width:30%;
+    height:100%;
+
+    min-width: 400px;
+    display: flex;
+    font-size: 20px;
+    color:white;
+}
+ol {
+    padding: 0;
+    color:white;
+    width:fit-content;
+}
+
+li {
+    list-style: decimal;
+    margin-bottom: 5px;
+}
+
+button{
+    background-color: transparent;
+    cursor: pointer;
+    border:none;
+    font-size: 30px;
+    color:red;
+}
+</style>
