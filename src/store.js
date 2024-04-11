a89fc69fe53626fb6a108cb9b22a0aa48ce26ca6
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-04-12 00:47:37 +0200

    Initial Commit

---
+++ src/store.js
@@ -0,0 +1,20 @@
+// src/store/index.ts
+import { defineStore } from 'pinia'
+
+
+
+export const useStore  = defineStore('store', {
+    state: () => ({
+        stores: [] // Array to hold the list of stores
+    }),
+    actions: {
+        async fetchStores() {
+            try {
+                const response = await fetch('http://localhost:3000/stores');
+                this.stores = await response.json();
+            } catch (error) {
+                console.error('Error fetching stores:', error);
+            }
+        }
+    }
+})
