4ce83bbf78a41309e80d4c7d8d96be16097769d5
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-04-13 16:22:11 +0200

    User Login Functional

--- phase-3/src/store.js
+++ phase-3/src/store.js
@@ -5,7 +5,8 @@ import { defineStore } from 'pinia'
 
 export const useStore  = defineStore('store', {
     state: () => ({
-        stores: [] // Array to hold the list of stores
+        stores: [] ,// Array to hold the list of stores
+        currentUser : ""
     }),
     actions: {
         async fetchStores() {
@@ -17,6 +18,37 @@ export const useStore  = defineStore('store', {
             } catch (error) {
                 console.error('Error fetching stores:', error);
             }
-        }
+        },
+        async loginUser(username,id,password,role) {
+            try {
+                const response = await fetch(`http://localhost:3000/user/${username}`);
+                this.currentUser = await response.json();
+                console.log('Current user:', this.currentUser);
+            } catch (error) {
+                console.error('Error fetching user:', error);
+                const userdata = {
+                    id: id,
+                    username: username,
+                    password: password,
+                    role: role
+                }
+                await this.registerUser(userdata);
+            }
+        },
+        async registerUser(userData) {
+            try {
+                const response = await fetch('http://localhost:3000/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(userData)
+                });
+               this.currentUser = await response.json();
+                console.log('Newly registered user:', this.currentUser);
+            } catch (error) {
+                console.error('Error registering user:', error);
+            }
+        },
     }
 })
